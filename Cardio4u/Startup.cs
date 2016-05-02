9afea18b8489e584117398a6034f663dee7885e7
DevEnv <DevEnv@DESKTOP-3VF3JBP>
2016-05-02 19:10:11 +0300

    initial commit

---
+++ Cardio4u/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cardio4u.Startup))]
+namespace Cardio4u
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
